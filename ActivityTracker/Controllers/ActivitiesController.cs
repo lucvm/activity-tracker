12859a90bb8738262abf85c952a1433006825c99
Luc van Montfort <lucvanmontfort@gmail.com>
2018-01-13 02:06:59 +0100

    Fix no notes error

--- ActivityTracker/Controllers/ActivitiesController.cs
+++ ActivityTracker/Controllers/ActivitiesController.cs
@@ -67,11 +67,15 @@ namespace ActivityTracker.Controllers
                 throw new ApplicationException("Not a valid user, try again with another account");
             }
 
-            var activities = GetAllActivities().Where(a => a.ApplicationUserID == studentId).ToList();
+            var activities = GetAllActivities().Where(a => a.ApplicationUserID == studentId).
+                OrderBy(a => a.Complete).ToList();
 
             foreach (var activity in activities)
             {
-                activity.Notes = activity.Notes.Split(new[] { '.', '\r', '\n' }).FirstOrDefault() + '.';
+                if (activity.Notes != null)
+                {
+                    activity.Notes = activity.Notes.Split(new[] { '.', '\r', '\n' }).FirstOrDefault() + '.';
+                }
             }
 
             ViewBag.Activities = activities;
